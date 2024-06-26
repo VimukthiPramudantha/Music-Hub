b0c785c577dc92719a01bd5e5eeec13c7b216d61
VimukthiPramudantha <vimukthipramudanth@gmail.com>
2024-06-26 21:16:45 +0530

    upgrade

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const toggleButton = document.getElementById('darkmood');
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+document.addEventListener("DOMContentLoaded", (event) => {
+  const toggleButton = document.getElementById("darkmood");
+  const prefersDarkScheme = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
 
-    // Load saved theme preference
-    const currentTheme = localStorage.getItem('theme') || (prefersDarkScheme ? 'dark' : 'light');
+  const currentTheme =
+    localStorage.getItem("theme") || (prefersDarkScheme ? "dark" : "light");
 
-    if (currentTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-    }
+  if (currentTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
 
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        
-        const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
-        localStorage.setItem('theme', theme); // Save preference
-    });
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    const theme = document.body.classList.contains("dark-mode")
+      ? "dark"
+      : "light";
+    localStorage.setItem("theme", theme);
+  });
 });
