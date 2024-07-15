cbbe3bc3c3b1af939210bcf3e1ce7af9b5a80e48
Vimukthi pramudantha <159423933+VimukthiPramudantha@users.noreply.github.com>
2024-07-15 22:25:50 +0530

    added icons to dark theme button

--- EnglishSongs/javaScript.js
+++ EnglishSongs/javaScript.js
@@ -3,11 +3,16 @@ var isDarkMode = false;
 function changeB() {
   var body = document.body;
   isDarkMode = !isDarkMode;
+  var img = document.getElementById('navImg');
+
   if (isDarkMode) {
+    img.setAttribute('src','./img/icons8-partly-cloudy-day-50 (1).png');
     body.classList.remove("light-mode");
     body.classList.add("dark-mode");
   } else {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
+    img.setAttribute('src','./img/icons8-moon-30.png');
   }
+
 }
