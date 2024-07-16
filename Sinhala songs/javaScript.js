9e666184f246518115f414bd719dd7dac7f4ba5d
Vimukthi pramudantha <159423933+VimukthiPramudantha@users.noreply.github.com>
2024-07-16 17:17:49 +0530

    Dark mode to Sinhala songs page

---
+++ Sinhala songs/javaScript.js
@@ -0,0 +1,18 @@
+var isDarkMode = false;
+
+function changeB() {
+  var body = document.body;
+  isDarkMode = !isDarkMode;
+  var img = document.getElementById('navImg');
+
+  if (isDarkMode) {
+    img.setAttribute('src','./icons8-partly-cloudy-day-50 (1).png');
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
+  } else {
+    body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
+    img.setAttribute('src','./icons8-moon-30.png');
+  }
+
+}
