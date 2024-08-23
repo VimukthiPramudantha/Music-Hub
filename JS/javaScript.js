732ebcaa3a21a622f4be3d44cfd0df15f7b15c0e
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2024-08-23 17:50:02 +0530

    making some changes

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -3,16 +3,15 @@ var isDarkMode = false;
 function changeB() {
   var body = document.body;
   isDarkMode = !isDarkMode;
-  var img = document.getElementById('navImg');
+  var img = document.getElementById("navImg");
 
   if (isDarkMode) {
-    img.setAttribute('src','./icons8-partly-cloudy-day-50 (1).png');
+    img.setAttribute("src", "../images/icons8-partly-cloudy-day-50 (1).png");
     body.classList.remove("light-mode");
     body.classList.add("dark-mode");
   } else {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
-    img.setAttribute('src','./icons8-moon-30.png');
+    img.setAttribute("src", "../images/icons8-moon-30.png");
   }
-
 }
