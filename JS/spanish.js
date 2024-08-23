a7877085e666b2cb34bcd04262278a1ccfa8a239
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2024-08-23 18:04:44 +0530

    relocated files

--- JS/spanish.js
+++ JS/spanish.js
@@ -6,13 +6,13 @@ function changeB() {
   var img = document.getElementById('navImg');
 
   if (isDarkMode) {
-    img.setAttribute('src','./images/icons8-partly-cloudy-day-50 (1).png');
+    img.setAttribute('src','../images/icons8-partly-cloudy-day-50 (1).png');
     body.classList.remove("light-mode");
     body.classList.add("dark-mode");
   } else {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
-    img.setAttribute('src','./images/icons8-moon-30.png');
+    img.setAttribute('src','../images/icons8-moon-30.png');
   }
 
 }
